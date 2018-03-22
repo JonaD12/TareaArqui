6d434015b80953bfda93e916557aa29d3668427b
JonaD12 <35873930+JonaD12@users.noreply.github.com>
2018-03-22 11:28:30 -0600

    ajuste de php
    agrege el direccionamiento autoatico, es decir despues de 5seg de mostrar la respuesta lo redirige automaticamente a la zona del sistema para realizar otra prueba

--- ejercicio1/operacion.php
+++ ejercicio1/operacion.php
@@ -6,5 +6,12 @@
 $result = $num1 / $num2;
 echo "La velocidad es de: ".$result;
 
-
+<script type="text /javascript">
+function delayedRedirect()
+{
+	window.location.href = "index.php"
+}
+</script>
+<body onload="setTimeout('delayedRedirect()', 5000)">
+</body>
  ?>
